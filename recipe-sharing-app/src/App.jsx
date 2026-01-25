ba0887f34cb2b9385d0a64c277cb2c31f32d81bb
ovieiteke <ovie.iteke@gmail.com>
2026-01-25 23:11:11 +0100

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm'
+import AddRecipeForm from './components/AddRecipeForm.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
