388999b89093ade3170abd0ed245347cefc2b024
ovieiteke <ovie.iteke@gmail.com>
2026-01-27 17:17:45 +0100

    App.jsx update

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './Counter'
 
 
 function App() {
@@ -42,7 +43,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      
+      <div>
+        <Counter />
+      </div>
     </>
   )
 }
