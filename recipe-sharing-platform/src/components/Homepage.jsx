dfb174771b52f723194e0466f8ef054f42131bd2
ovieiteke <ovie.iteke@gmail.com>
2026-02-15 21:41:15 +0100

    Updated HomePage

--- recipe-sharing-platform/src/components/Homepage.jsx
+++ recipe-sharing-platform/src/components/Homepage.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import DataFetcher from "../App";
 
+function DataFetcher() {
+  const [data, setData] = useState([]);
+      const [loading, setLoading] = useState(true);
+  
+      useEffect(() => {
+              const fetchData = async () => {
+                  try {
+                      const response = await fetch('./data.json');
+                      const jsonData = await response.json();
+                      setData(jsonData);
+                      setLoading(false);
+                  } catch (error) {
+                      console.error("Error fetching data: ", error);
+                      setLoading(false);
+                  }
+              };
+              fetchData();
+            }, []);
+          };
+
+
 function Homepage() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-blue-100">
