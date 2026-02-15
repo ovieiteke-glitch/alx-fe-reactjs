7997ba80861556bfadc663e6d01538cad0fdc172
ovieiteke <ovie.iteke@gmail.com>
2026-02-15 19:26:29 +0100

    Homepage created

---
+++ recipe-sharing-platform/src/components/Homepage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import DataFetcher from "../App";
+
+function Homepage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-red-100">
+            <header>
+                <h1>Welcome to my page!</h1>
+            </header>
+            <body>
+                <p>Hi I am Ovie and this is my page.</p>
+                <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+                    <DataFetcher />
+                </div>
+                
+            </body>
+            <footer>
+                <p>&copy Oveewrites</p>
+            </footer>
+        </div>
+    )
+}
+
+
+export default Homepage;
---
+++ recipe-sharing-platform/src/components/data.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'; 
+function DataFetcher() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+            const fetchData = async () => {
+                try {
+                    const response = await fetch('./data.json');
+                    const jsonData = await response.json();
+                    setData(jsonData);
+                    setLoading(false);
+                } catch (error) {
+                    console.error("Error fetching data: ", error);
+                    setLoading(false);
+                }
+            };
+            fetchData();
+        }, []);
+}
+
+
