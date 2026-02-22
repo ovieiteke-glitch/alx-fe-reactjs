99f29479c3f83e40af3d8706e63016b834de61f3
ovieiteke <ovie.iteke@gmail.com>
2026-02-22 23:04:49 +0100

    Inputed validation logic

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -7,9 +7,35 @@ const ControlledForm = () => {
         setFormData(prevState => ({ ...prevState, [name]: value }));
     };
 
+    const validateForm = () => {
+         const newErrors = {};
+        if (!formData.username.trim()) { // Use .trim() to check for only whitespace
+        newErrors.username = 'Username is required';
+    }
+        if (!formData.email.trim()) {
+        newErrors.email = 'Email is required';
+    }
+        if (!formData.password.trim()) {
+        newErrors.password = 'Password is required';
+    } 
+        return newErrors;
+    };
+
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(formData);
+        const formErrors = validateForm();
+        if (Object.keys(formErrors).length === 0) {
+    // No errors, submit the form data (e.g., to an API)
+    alert('Form submitted successfully!');
+    console.log('Form data:', formData);
+    setErrors({}); // Clear errors after successful submit
+  } else {
+    // Found errors, update the errors state to display messages
+    setErrors(formErrors);
+  }
     };
 
     return (
